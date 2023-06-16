49105ae6d2ddc213b2be48d8a8cd157e7c12e71c
IvetteLara <ivette.lara2012@gmail.com>
2023-06-15 21:16:03 -0600

    Table register_business

--- domain/src/schema.rs
+++ domain/src/schema.rs
@@ -1,9 +1,33 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    ecommerce.tests (id) {
+    register_business (id) {
         id -> Int4,
-        codigo -> Varchar,
-        nombre -> Varchar,
+        #[max_length = 100]
+        app_name -> Varchar,
+        app_logo -> Bytea,
+        #[max_length = 100]
+        app_url -> Varchar,
+        #[max_length = 32]
+        api_id -> Varchar,
+        #[max_length = 32]
+        api_secret -> Varchar,
+        notify_customer -> Nullable<Bool>,
+        notify_email -> Nullable<Bool>,
+        #[max_length = 100]
+        set_emails -> Varchar,
+        notify_webhook -> Nullable<Bool>,
+        #[max_length = 100]
+        set_webhook -> Varchar,
+        #[max_length = 100]
+        link_url_pay -> Nullable<Varchar>,
+        link_timeout -> Nullable<Int4>,
+        link_amout -> Nullable<Bool>,
+        link_count -> Nullable<Bool>,
+        ask_name -> Nullable<Bool>,
+        ask_mobile -> Nullable<Bool>,
+        ask_email -> Nullable<Bool>,
+        ask_address -> Nullable<Bool>,
     }
 }
+
